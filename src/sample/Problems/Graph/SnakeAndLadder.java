9c044297080a0932bbd03586fac666931ca505d5
nijindal <nijindal@expediagroup.com>
2020-10-31 19:55:32 +0530

    added packages

---
+++ src/sample/Problems/Graph/SnakeAndLadder.java
@@ -0,0 +1,18 @@
+package sample.Problems.Graph;
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main (String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+
+        while(t != 0) {
+
+            t--;
+        }
+    }
+}
