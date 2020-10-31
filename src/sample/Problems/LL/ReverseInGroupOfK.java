c54f828b79b18b7279568a8e219d1985b27e34a7
nijindal <nijindal@expediagroup.com>
2020-11-01 00:44:12 +0530

    dp problem

---
+++ src/sample/Problems/LL/ReverseInGroupOfK.java
@@ -0,0 +1,19 @@
+package sample.Problems.LL;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+public class ReverseInGroupOfK {
+    public static Node reverse(Node node, int k)
+    {
+        return node;
+    }
+}
