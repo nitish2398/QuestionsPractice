800638eb797140148dbd71a3386ecf5260318eeb
nijindal <nijindal@expediagroup.com>
2020-11-01 01:04:17 +0530

    reverse LL

--- src/sample/Problems/LL/ReverseInGroupOfK.java
+++ src/sample/Problems/LL/ReverseInGroupOfK.java
@@ -1,5 +1,15 @@
 package sample.Problems.LL;
 
+/**
+ * Reverse a Linked List in groups of given size.
+ *
+ * Input:
+ * LinkedList: 1->2->2->4->5->6->7->8
+ * K = 4
+ * Output: 4 2 2 1 8 7 6 5
+ *
+ */
+
 class Node
 {
     int data;
@@ -14,6 +24,23 @@ class Node
 public class ReverseInGroupOfK {
     public static Node reverse(Node node, int k)
     {
-        return node;
+        int count = 0;
+        Node prev = null;
+        Node next = null;
+        Node current = node;
+
+        while(current != null && count < k) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+            count++;
+        }
+
+        if(next != null) {
+            node.next = reverse(next, k);
+        }
+
+        return prev;
     }
 }
