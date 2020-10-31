0a05c189f727519593174eb7d84d13cc494e98dd
nijindal <nijindal@expediagroup.com>
2020-10-31 19:58:27 +0530

    add snake and ladder

--- src/sample/Problems/Graph/SnakeAndLadder.java
+++ src/sample/Problems/Graph/SnakeAndLadder.java
@@ -3,14 +3,19 @@ package sample.Problems.Graph;
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class GFG
-{
-    public static void main (String[] args)
-    {
+
+/**
+ * Given a snake and ladder board of order 5x6,
+ * find the minimum number of dice throws required to reach
+ * the destination or last cell (30th cell) from source (1st cell) .
+ */
+
+class GFG {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int t = scan.nextInt();
 
-        while(t != 0) {
+        while (t != 0) {
 
             t--;
         }
