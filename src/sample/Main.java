94ae67c483f574d9584ccebbe3419b3e48fdc86d
nijindal <nijindal@expediagroup.com>
2020-11-17 00:00:26 +0530

    add knapsack

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -277,11 +277,6 @@ class Sum1 {
 }
 
 class GFG1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println(palindromicNumbers(n));
-    }
 
     static int palindromicNumbers(int n) {
         if (n <= 10) {
